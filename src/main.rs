6e85b39ff8b1a89a6b7bb9313de7ba73009783a0
Stanislav Shandyga <stanislavshandyga@gmail.com>
2018-10-15 12:08:41 +0300

    Add thread pool abstraction

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+extern crate druste;
+use druste::ThreadPool;
 use std::fs;
 use std::io::prelude::*;
 use std::net::TcpListener;
@@ -7,11 +9,12 @@ use std::time::Duration;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(4);
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        thread::spawn(|| {
+        pool.execute(|| {
             handle_connection(stream);
         });
     }
