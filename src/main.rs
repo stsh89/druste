6fe62ccbca56881bdf5d8b80f9f7db30eebf83a8
Stanislav Shandyga <stanislavshandyga@gmail.com>
2018-10-15 11:52:55 +0300

    Minor refactoring

--- src/main.rs
+++ src/main.rs
@@ -16,19 +16,15 @@ fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 512];
     stream.read(&mut buffer).unwrap();
 
-    let get = b"GET / HTTP/1.1\r\n";
-
-    if buffer.starts_with(get) {
-        let contents = fs::read_to_string("index.html").unwrap();
-        let response = format!("HTTP/1.1 200 OK\r\n\r\n{}", contents);
-
-        stream.write(response.as_bytes()).unwrap();
-        stream.flush().unwrap()
+    let (status_line, filename) = if buffer.starts_with(b"GET / HTTP/1.1\r\n") {
+        ("HTTP/1.1 200 OK\r\n\r\n", "index.html")
     } else {
-        let contents = fs::read_to_string("404.html").unwrap();
-        let response = format!("HTTP/1.1 404 NOT FOUND\r\n\r\n{}", contents);
+        ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "404.html")
+    };
 
-        stream.write(response.as_bytes()).unwrap();
-        stream.flush().unwrap()
-    }
+    let contents = fs::read_to_string(filename).unwrap();
+    let response = format!("{}{}", status_line, contents);
+
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap()
 }
